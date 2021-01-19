8e57395d3506314e1979a772e5b75ee8faba8053
Mateusz Gajewski <mateuszgajewski@gmail.com>
2021-01-19 11:41:16 +0100

    21.01.19

---
+++ setExpression.py
@@ -0,0 +1,25 @@
+names = {"arkadiusz", "Wioletta", "karol", "bartlomiej", "Jakub", "Anna"}
+
+
+names2 =    {
+    name.capitalize()
+    for name in names        
+            }
+
+names3 =    {
+    name.capitalize()
+    for name in names
+    if not (name.startswith("B") or name.startswith("b"))
+            }
+
+
+
+
+'''
+names =         {
+    names.discard(name)
+    for name in names
+    if (name.startswith("B") or name.startswith("b"))
+                }
+'''
+#set nie moze zmienić rozmiaru
