58bfd3926f75412159d0b1292a2c2212c5dd142c
Mateusz Gajewski <mateuszgajewski@gmail.com>
2021-01-19 11:57:38 +0100

    21.01.19

--- setExpression.py
+++ setExpression.py
@@ -1,6 +1,6 @@
 names = {"arkadiusz", "Wioletta", "karol", "bartlomiej", "Jakub", "Anna"}
 
-'''
+
 names2 =    {
     name.capitalize()
     for name in names        
@@ -12,14 +12,12 @@ names3 =    {
     if not (name.startswith("B") or name.startswith("b"))
             }
 
-
 '''
-
-
 names =         {
     names.discard(name)
     for name in names
     if (name.startswith("B") or name.startswith("b"))
-                }
+               }
+'''
 
-#set nie moze zmienić rozmiaru
+#nie mozna uzyc discard z forem na secie
